60a9797e2b89702279f3f3d7dbbb25a8af3ec3f4
ramezcontacts <ram@ezcontacts.com>
2023-06-14 17:32:17 +0530

    Iniital Commit1

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,3 @@
-import '../styles/bootstrap.min.css'
-import '../styles/globals.css'
-// import '../styles/index.css';
-import '../styles/new-index.css';
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import '../components/accordion/Accordion.css';
-import { config } from "@fortawesome/fontawesome-svg-core";
-import '../components/accordion/Accordion.css';
-config.autoAddCss = false;
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
